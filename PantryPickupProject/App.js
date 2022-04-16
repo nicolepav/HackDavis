a94c07fa9f2b716f604dd5cfe5d6ec3a89443a5e
nicolepav <nicolepav2.0@gmail.com>
2022-04-16 16:21:41 -0700

    nav setup

--- PantryPickupProject/App.js
+++ PantryPickupProject/App.js
@@ -1,12 +1,21 @@
+import * as React from 'react';
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Provider as PaperProvider } from 'react-native-paper'
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+       <Stack.Navigator>
+         <Stack.Screen name="Home" component={HomeScreen} />
+       </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,3 +27,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// <Stack.Navigator>
+// {/* <Stack.Screen name="Home" component={HomeScreen} />
+// </Stack.Navigator> */}
+
+ // <Stack.Navigator>
+      //   <Stack.Screen name="Home" component={HomeScreen} />
+      // </Stack.Navigator>
