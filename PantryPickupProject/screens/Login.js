586fed733c23d163ac19d44afe8b0ac5e7d2c928
nicolepav <nicolepav2.0@gmail.com>
2022-04-16 18:01:08 -0700

    login page style adjustments

--- PantryPickupProject/screens/Login.js
+++ PantryPickupProject/screens/Login.js
@@ -1,46 +1,62 @@
 import React from 'react';
-import { ScrollView, StyleSheet, TextInput, View, Text, TouchableOpacity } from 'react-native';
-import { Button, Card } from 'react-native-paper';
-import { DefaultTheme } from 'react-native-paper';
-
+import { StyleSheet, Image, View, Text, TouchableOpacity } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
 
 function Login ( {navigation} ) {
-  return (
- 
-    <View style={styles.container}>
-       <View style={styles.textWrapper}>
-        <Text style={styles.loginWelcome}>
-                WELCOME
-            </Text>
-            <Text style={styles.plainText}>
-                Please login with your UC Davis email
-            </Text>
-        </View> 
-       
-        <View>
-            <TouchableOpacity style={styles.button} mode="contained" onPress={() => navigation.navigate('SecondScreen')}>
-                <Text style={styles.buttonText} >
-                    CAS LOGIN
-                </Text>
-            </TouchableOpacity>
-        </View>
-    </View>
-    
 
+    const [emailtext, onChangeText] = React.useState("enter email address");
+    const [number, onChangeNumber] = React.useState(null);
+
+    return (
     
-  )
+        <View style={styles.container}>
+            <Image 
+                source={require('../assets/images/pantryLogoBig.png')} 
+                style={{ width: '216px', height: '216px', marginBottom: '50px' }}
+            />
+            
+            <View style={styles.textWrapper}>
+                <Text style={styles.loginWelcome}>
+                    WELCOME
+                </Text>
+                
+            </View> 
+        
+            <View style={styles.resetView}>
+                <Text style={styles.plainText}>
+                    UC Davis Email
+                </Text>
+                <TextInput
+                    value={emailtext}
+                    style={styles.input}
+                    onChangeText={onChangeText}
+                />
+                <TouchableOpacity style={styles.button} mode="contained" onPress={() => navigation.navigate('SecondScreen')}>
+                    <Text style={styles.buttonText} >
+                        CAS LOGIN
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+        
+
+        
+    )
 }
 
 const styles = StyleSheet.create({
+    
+    resetView: {
+        padding: 0,
+        margin: 0,
+    },
     container: {
         flex: 1,
         flexDirection: 'column',
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-        borderColor: 'black',
-        borderWidth: 1,
     },
 
     image :{
@@ -81,10 +97,23 @@ const styles = StyleSheet.create({
         fontWeight: 500,
         fontSize: '14px',
         lineHeight: '20px',
-        color: "#000000",
-        margin: '20px',
+        color: "#606060",
+        marginTop: '20px',
+        marginBottom: '0px',
+        textAlign: 'left',
+        width: '80vw',
+        borderWidth: 1,
     },
-   
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 15,
+        width: '80vw',
+        height: '5vh',
+        color: "#606060"
+      },
 
      
 });
