94e6cb190f36c64830cdec4708fb3f91b77ac405
nicolepav <nicolepav2.0@gmail.com>
2022-04-16 17:30:13 -0700

    delete messup project

--- PantryPickupProject/screens/Login.js
+++ PantryPickupProject/screens/Login.js
@@ -9,13 +9,18 @@ function Login ( {navigation} ) {
   return (
  
     <View style={styles.container}>
-        
+       <View style={styles.textWrapper}>
         <Text style={styles.loginWelcome}>
-            WELCOME
-        </Text>
+                WELCOME
+            </Text>
+            <Text style={styles.plainText}>
+                Please login with your UC Davis email
+            </Text>
+        </View> 
+       
         <View>
             <TouchableOpacity style={styles.button} mode="contained" onPress={() => navigation.navigate('SecondScreen')}>
-                <Text>
+                <Text style={styles.buttonText} >
                     CAS LOGIN
                 </Text>
             </TouchableOpacity>
@@ -34,6 +39,8 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        borderColor: 'black',
+        borderWidth: 1,
     },
 
     image :{
@@ -47,21 +54,37 @@ const styles = StyleSheet.create({
         marginLeft: 20,
     },
     button: {
-        backgroundColor: "#FFBF1C",
+        backgroundColor: "#F7B257",
         textAlign: 'center',
-        padding: 10,
-        paddingLeft: 40,
-        paddingRight: 40,
         borderRadius: 15,
+        width: '80vw',
+        height: '5vh',
+        justifyContent: 'center',
     },
     loginWelcome: {
-        fontFamily: 'Montserrat',
+        fontFamily: 'Montserrat-Bold.ttf',
         fontWeight: 700,
         fontSize: '32px',
         lineHeight: '39px',
         textAlign: 'center',
-        color: "#F7B156",
-    }
+        color: "#F7B257",
+    },
+    buttonText: {
+        fontFamily: 'Montserrat-Bold.ttf',
+        fontWeight: 700,
+        fontSize: '20px',
+        lineHeight: '16px',
+        color: "#FFFFFF"
+    },
+    plainText: {
+        fontFamily: 'Montserrat.ttf',
+        fontWeight: 500,
+        fontSize: '14px',
+        lineHeight: '20px',
+        color: "#000000",
+        margin: '20px',
+    },
+   
 
      
 });
